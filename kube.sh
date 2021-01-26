24faa4895362b2edae3da974677f1d7d70982b61
Supakorn Trakulmaiphol (AP) <s.trakulmaiphol@global.ntt>
2021-01-26 17:41:47 +0700

    [FIX] Modprobe routing Kubernetes

--- kube.sh
+++ kube.sh
@@ -7,17 +7,26 @@ export OS=xUbuntu_20.04
 
 apt-get update -y
 
+swapoff --all
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+# Create the .conf file to load the modules at bootup
+cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
+overlay
 br_netfilter
 EOF
 
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set up required sysctl params, these persist across reboots.
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
 net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
 EOF
+
 sudo sysctl --system
 
 apt-get install firewalld -y
